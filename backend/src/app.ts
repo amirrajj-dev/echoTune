9ff469cf48a5d8a05cc3aba231e77e01cb4c99e2
amirrajj-dev <amiramraja@gmail.com>
2025-05-26 13:34:15 +0330

    little backend change🎵💙

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -38,7 +38,8 @@ app.use(helmet({
 }));
 app.use(morgan('dev'));
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173'
+  origin: process.env.FRONTEND_URL || 'http://localhost:5173',
+  credentials: true
 }));
 
 app.use('/api/users', userRoutes);
