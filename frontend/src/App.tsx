42636f8d7194ad0243213eac0fa5a6a9dff77768
amirrajj-dev <amiramraja@gmail.com>
2025-05-26 13:35:27 +0330

    added some of the starting routes🎵💙

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,17 @@
-import { SignedIn, SignedOut, SignInButton, UserButton } from "@clerk/clerk-react";
-import React from "react";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/home/HomePage";
+import AuthCallbackPage from "./pages/authCallBack/AuthCallbackPage";
+import { AuthenticateWithRedirectCallback } from "@clerk/clerk-react";
 
 const App = () => {
   return (
-    <header>
-      <SignedOut>
-        <button className="btn btn-primary m-4">
-        <SignInButton />
-        </button>
-      </SignedOut>
-      <SignedIn>
-        <UserButton />
-      </SignedIn>
-    </header>
+    <div className="font-mono transition-all h-screen p-4 bg-gradient-to-br from-primary to-secondary">
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/auth-callback" element={<AuthCallbackPage />} />
+        <Route path="/sso-callback" element={<AuthenticateWithRedirectCallback signUpFallbackRedirectUrl={'/auth-callback'} />} />
+      </Routes>
+    </div>
   );
 };
 
