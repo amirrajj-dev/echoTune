a1227a15e0ae34ae6cbd981f6592a9aef611b3e7
amirrajj-dev <amiramraja@gmail.com>
2025-05-30 16:44:23 +0330

    fixed little mistakes🎵💙

--- backend/src/routes/stat.route.ts
+++ backend/src/routes/stat.route.ts
@@ -1,8 +1,9 @@
 import express from 'express'
 import { protectRoute, requireAdmin } from '../middlewares/auth.middleware';
+import { getStats } from '../controllers/stat.controller';
 
 const router = express.Router()
 
-router.get('/' , protectRoute as express.RequestHandler , requireAdmin as express.RequestHandler);
+router.get('/' , protectRoute as express.RequestHandler , requireAdmin as express.RequestHandler , getStats as express.RequestHandler);
 
 export default router;
